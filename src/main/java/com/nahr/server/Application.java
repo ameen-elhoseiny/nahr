1a5ff2852bab182cd63b999667e649804efda534
ameen-elhoseiny <ameen.el@gmail.com>
2018-08-05 20:23:35 +0200

    first run no code. installation instruction

---
+++ src/main/java/com/nahr/server/Application.java
@@ -0,0 +1,15 @@
+package com.nahr.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ameen on 05/08/2018.
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
